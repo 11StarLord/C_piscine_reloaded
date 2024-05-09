4237d44a3f9fa70304cf9f724e4bb55cf9734f8b
11StarLord <diogowalter114@gmail.com>
2024-05-09 01:32:46 +0100

    add ex21

---
+++ ex21/ft_range.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+int	*ft_range(int min, int max);
+
+int	*ft_range(int min, int max)
+{
+	int	i;
+	int	*result;
+
+	if (min >= max)
+	{	
+		return (0);
+	}
+	i = max - min;
+	result = (int *)malloc(sizeof(int) * (i));
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (max > min)
+	{
+		result[i] = min;
+		min++;
+		i++;
+	}
+	return (result);
+}
